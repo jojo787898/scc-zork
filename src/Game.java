3b0b441475e20cc64dc4783e7b0bcd97dafec023
Riley Oblander <rileyjoblander@gmail.com>
2020-10-29 18:17:42 -0700

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 public class Game {
     public static void main(String[] args) throws IOException {
 
+        // Creates a new map with one empty room called Test Room
         ArrayList<Room> rooms = new ArrayList<Room>();
 
         rooms.add(new Room("Test Room","Test room description", new Container(), new ArrayList<Container>(), new ArrayList<Door>()));
 
         Map map = new Map(rooms);
 
+        // Saves the map to a JSON file
         for (Room room:map.rooms) {
             room.toJSON();
         }
         System.out.println(map.toString());
 
+        // Loads a new map from that JSON file
         Map map2 = new Map("Test Room");
 
         System.out.println(map2.toString());
