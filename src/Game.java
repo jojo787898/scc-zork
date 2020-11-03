0bde5a81bbd8281b8b5a33d97ff386645946b3ce
Riley Oblander <rileyjoblander@gmail.com>
2020-11-02 18:12:28 -0800

    Add files via upload

--- src/Game.java
+++ src/Game.java
@@ -8,16 +8,24 @@ public class Game {
         // Creates a new map with one empty room called Test Room
         ArrayList<Room> rooms = new ArrayList<Room>();
 
-        rooms.add(new Room("Test Room","Test room description", new Container(), new ArrayList<Container>(), new ArrayList<Door>()));
+        //Uses the constructor for an unconnected room
+        Room room1 = new Room("Test Room","Test room description", new Container(), new ArrayList<Container>(), new ArrayList<Door>());
 
-        rooms.add(new Room("Test Room 2","Test room description 2", new Container(), new ArrayList<Container>(), new ArrayList<Door>()));
+        //Uses the constructor to create a new room linked to an existing room, with default door state open and unlocked
+        Room room2 = new Room("Test Room 2","Test room description 2", new Container(), new ArrayList<Container>(),room1);
+
+
+        rooms.add(room1);
+        rooms.add(room2);
+
         Map map = new Map(rooms);
+
+        System.out.println(map.toString());
 
         // Saves the map to a JSON file
         for (Room room:map.rooms) {
             room.toJSON();
         }
-        System.out.println(map.toString());
 
         // Loads a new map from that JSON file
         Map map2 = new Map("Map/");
--- src/Room.java
+++ src/Room.java
@@ -14,6 +14,7 @@ public class Room {
     ArrayList<Container> containers;
     ArrayList<Door> doors;
 
+    //Creates a room with all given values
     public Room(String name, String description, Container items, ArrayList<Container> containers, ArrayList<Door> doors){
 
         this.name = name;
@@ -23,6 +24,36 @@ public class Room {
         this.doors = doors;
     }
 
+    //Creates a room connected to an existing room with a default door that is open and unlocked
+    public Room(String name, String description, Container items, ArrayList<Container> containers, Room neighbor){
+
+        this.name = name;
+        this.description = description;
+        this.items = items;
+        this.containers = containers;
+        doors = new ArrayList<>();
+        Door temp = new Door("Door",false, true, new Room[]{this,neighbor});
+        doors.add(temp);
+        neighbor.addDoor(temp);
+    }
+
+    //Creates a room connected to an existing room with a custom door state
+    public Room(String name, String description, Container items, ArrayList<Container> containers, Room neighbor, Boolean open, Boolean locked){
+
+        this.name = name;
+        this.description = description;
+        this.items = items;
+        this.containers = containers;
+        doors = new ArrayList<>();
+        Door temp = new Door("Door",open, locked, new Room[]{this,neighbor});
+        doors.add(temp);
+        neighbor.addDoor(temp);
+    }
+
+
+    public void addDoor(Door door){
+        doors.add(door);
+    }
 
     public void toJSON() throws IOException {
         File directory = new File("Map");
@@ -36,3 +67,4 @@ public class Room {
         myWriter.close();
     }
 }
+                                                  
